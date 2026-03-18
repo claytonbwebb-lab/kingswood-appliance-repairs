edda538779f94205c1521c2feca33efd8fdf6c9d
Ubuntu <ubuntu@ip-172-31-16-190.eu-north-1.compute.internal>
2026-03-18 21:10:43 +0000

    Fix: Hi I'm David not Dave

--- make-david.py
+++ make-david.py
@@ -17,7 +17,7 @@ van_photo = van_crop.resize((photo_w, photo_h), Image.LANCZOS)
 # Polaroid: white surround with generous bottom for the text
 border_top = 14
 border_sides = 14
-border_bottom = 58  # space for "Hi, I'm Dave" text
+border_bottom = 58  # space for "Hi, I'm David" text
 
 polaroid_w = photo_w + border_sides * 2
 polaroid_h = photo_h + border_top + border_bottom
@@ -29,13 +29,13 @@ polaroid.paste(van_photo, (border_sides, border_top))
 draw_p = ImageDraw.Draw(polaroid)
 draw_p.rectangle([0, 0, polaroid_w-1, polaroid_h-1], outline=(210, 210, 210, 255), width=1)
 
-# Add "Hi, I'm Dave" text in the white space below the photo
+# Add "Hi, I'm David" text in the white space below the photo
 try:
     font = ImageFont.truetype('/tmp/Pacifico.ttf', 28)
 except:
     font = ImageFont.load_default()
 
-text = "Hi, I'm Dave"
+text = "Hi, I'm David"
 bbox = draw_p.textbbox((0, 0), text, font=font)
 text_w = bbox[2] - bbox[0]
 text_x = (polaroid_w - text_w) // 2
